53c6ea6380d424882a73e56fdc36d03167896312
kristannbads <kristbadua@gmail.com>
2023-08-19 08:42:29 +0800

    Add separate serializer for image and upload image function

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,10 +7,10 @@ from recipe.serializers import (
     TagSerializer,
     IngredientSerializer)
 from rest_framework import authentication, permissions
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from core.models import Recipe, Tag, Ingredient
-
-# Create your views here.
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -28,12 +28,27 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
 
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrviewSet(
