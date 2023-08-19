3a7f78e5a936ac0888e130eb601e732a2365074e
kristannbads <kristbadua@gmail.com>
2023-08-19 11:56:01 +0800

    Modify media and static root

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT = 'vol/web/media'
-STATIC_ROOT = 'vol/web/static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'vol/web/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'vol/web/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
